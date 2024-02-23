78c6d147de387529140b7cdea5eeb1b67bd4667b
andre-luiz-aquino <andre.luiz.aquino30@gmail.com>
2024-02-23 12:56:12 -0300

    Criando a classe Posicao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using tabuleiro;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Posicao P;
+
+        P = new Posicao(3, 4);
+
+        Console.WriteLine("Posição " +  P);
+
+        Console.WriteLine();
     }
 }
